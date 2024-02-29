c374c6d4b7bbf781cbd7a53cd24b05d21e28da1d
awehero <147273991+awehero@users.noreply.github.com>
2024-02-29 17:13:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let pops = 0;
 let nails = 0;
 function updatePopsCounter() {
   popsCounter.innerText = pops;
+  
 }
 document.getElementById('bubbleButton').addEventListener('click', function() {
   pops++;
