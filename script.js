6309a03e526617b53a97817078b334fd82262c6c
awehero <147273991+awehero@users.noreply.github.com>
2024-02-29 17:22:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ let nails = 0;
 function updatePopsCounterElement() {
   popsCounter.innerText = pops;
 }
-function updateUpgradeEements() {
+function updateUpgradeElements() {
   numberOfNails.innerText = nails;
 }
 document.getElementById('bubbleButton').addEventListener('click', function() {
   pops++;
   updatePopsCounterElement();
 });
+document.getElementById('nailsUpgradeButton').addEventListener('click' function() {
+  if (pops >= 10)
+  {
+    pops -= 10;
+    nails++;
+    updatePopsCounterElement();
+    updateUpgradeElements();
+  }
+};
